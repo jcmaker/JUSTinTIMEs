390b224dcbf12384941c573d8dcb815ab42167e2
jcmaker0627 <jcmaker0627@gmail.com>
2020-11-16 18:09:00 +0900

    login problem fixed (Home.js)

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import AppRouter from "./Router";
+import AppRouter from "components/Router";
 import { authService } from "fbManager";
 
 function App() {
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  <span>Home</span>;
+  return <span>Home</span>;
 };
 
 export default Home;
